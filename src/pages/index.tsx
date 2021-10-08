34c1dd66451a1e18b1e8d7788affb318322d4487
Foo-x <www.sand.x@gmail.com>
2021-10-08 20:06:56 +0900

    feat: append counter on click button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,51 @@
 import type { PageProps } from "gatsby"
-import React from "react"
+import React, { useReducer } from "react"
 import CounterWithLabel from "~/components/counterWithLabel"
 import * as styles from "~/styles/pages/index.module.css"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+type CounterProps = {
+  label: string
+  value: number
+}
+
+type Action = {
+  type: "append"
+  prop: CounterProps
+}
+
+const reducer = (state: CounterProps[], action: Action): CounterProps[] => {
+  switch (action.type) {
+    case "append":
+      state.push(action.prop)
+      return [...state]
+    default:
+      const _: never = action.type
+      return _
+  }
+}
+
 const IndexPage: React.FC<PageProps> = () => {
+  const [counterList, dispatch] = useReducer(reducer, [])
+
   return (
     <Layout>
       <Seo title="Home" />
       <div className={styles.counterList}>
-        <CounterWithLabel initLabel="foo" />
-        <CounterWithLabel initLabel="bar" />
-        <button className={styles.appendButton}></button>
+        {counterList.map((prop, i) => (
+          <CounterWithLabel
+            initLabel={prop.label}
+            initValue={prop.value}
+            key={i}
+          />
+        ))}
+        <button
+          className={styles.appendButton}
+          onClick={() =>
+            dispatch({ type: "append", prop: { label: "カウンタ", value: 0 } })
+          }
+        ></button>
       </div>
     </Layout>
   )
