e686e746bf6fcfb1c484184102ceaa7e19f54b54
Foo-x <www.sand.x@gmail.com>
2021-10-08 17:28:53 +0900

    feat: add append button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,21 @@
 import type { PageProps } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
+import CounterWithLabel from "~/components/counterWithLabel"
+import * as styles from "~/styles/pages/index.module.css"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage: React.FC<PageProps> = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-  </Layout>
-)
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <div className={styles.counterList}>
+        <CounterWithLabel initLabel="foo" />
+        <CounterWithLabel initLabel="bar" />
+        <button className={styles.appendButton}></button>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
